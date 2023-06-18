60012f8e06cdb9251f49b27938cd18df591c5688
Lazyprincessbot <127012156+Lazyprincessbot@users.noreply.github.com>
2023-06-18 11:39:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/75f58d091cccefade8d5b.jpg https://telegra.ph/file/c4ddf6a9d136cb1735bb1.jpg https://telegra.ph/file/b36685221ce5ac41ad667.jpg https://telegra.ph/file/7f59377ace528148d15bd.jpg https://telegra.ph/file/e006737306ad1c5c16192.jpg https://telegra.ph/file/f8b495d98fd4d89c99150.jpg https://telegra.ph/file/320cdc500bc7e3d1c9e94.jpg https://telegra.ph/file/90ea7771a7c61e2d45d72.jpg https://telegra.ph/file/0d6adc21a51a32c3ac803.jpg https://telegra.ph/file/3fd5587f5bf4c3c107c91.jpg https://telegra.ph/file/2182f0c156d4ae25c8913.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
@@ -65,7 +65,7 @@ SHORT_API = environ.get("SHORT_API")
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'jb_corporation')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ntmgroup')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 PM_IMDB = environ.get('PM_IMDB', "True")
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -81,16 +81,5 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
-
-#request force sub
-REQ_SUB = bool(environ.get("REQ_SUB", True))
-SESSION_STRING = environ.get("SESSION_STRING", "")
-
-
-
-
-
-
-
 
 
